e732689d9b0f2733937ef66ab15cf304f361a4d2
levancuon <levancuongprovip@gmail.com>
2024-07-04 17:05:55 +0700

    fix data user

--- src/main/java/org/example/fooballmanagerdn/models/User.java
+++ src/main/java/org/example/fooballmanagerdn/models/User.java
@@ -24,4 +24,27 @@ public class User {
     @Size(min=6,max=8)
     private String password;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

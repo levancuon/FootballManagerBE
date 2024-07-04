0439f39e77bc8ba8c02d4130076d00025ac2b50c
levancuon <levancuongprovip@gmail.com>
2024-07-04 13:32:19 +0700

    awjdkw

---
+++ src/main/java/org/example/fooballmanagerdn/FooballManagerDnApplication.java
@@ -0,0 +1,13 @@
+package org.example.fooballmanagerdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooballManagerDnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooballManagerDnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fooballmanagerdn/FooballManagerDnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fooballmanagerdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooballManagerDnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

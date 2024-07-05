71302431f024b18eb9083169c39a2eda957eea67
vansy61 <49103390+vansy61@users.noreply.github.com>
2024-07-06 00:39:10 +0700

    add method find by email user repo

--- src/main/java/org/example/footballmanagerdn/repositories/IUserRepo.java
+++ src/main/java/org/example/footballmanagerdn/repositories/IUserRepo.java
@@ -4,5 +4,8 @@ import org.example.footballmanagerdn.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface IUserRepo extends PagingAndSortingRepository<User,Long>, CrudRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
